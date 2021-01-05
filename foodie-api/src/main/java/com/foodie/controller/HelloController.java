f63a948bb6886dca4a9b40406cd1b3ab2bd32e17
BigOrange <bigorange.weicheng@gmail.com>
2021-01-05 08:10:18 +0800

    swagger 隐藏 HelloController

--- foodie-api/src/main/java/com/foodie/controller/HelloController.java
+++ foodie-api/src/main/java/com/foodie/controller/HelloController.java
@@ -4,7 +4,9 @@ import com.foodie.service.StuService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
+@ApiIgnore
 @RestController
 public class HelloController {
 
