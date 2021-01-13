a529b23da813432744496c32fdfa6bc18e07a0d7
BigOrange <bigorange.weicheng@gmail.com>
2021-01-13 17:04:50 +0800

    修改购物车类

--- foodie-api/src/main/java/com/foodie/controller/ShopcartController.java
+++ foodie-api/src/main/java/com/foodie/controller/ShopcartController.java
@@ -16,10 +16,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Api(value="购物车接口",tags = {"购物车相关接口"})
-@RestController
-public class ShopController {
+@RequestMapping("shopcart")
+public class ShopcartController {
 
-    final static Logger logger = LoggerFactory.getLogger(ShopController.class);
+    final static Logger logger = LoggerFactory.getLogger(ShopcartController.class);
 
     @Autowired
     StuService stuService;
@@ -38,6 +38,24 @@ public class ShopController {
         }
         System.out.println(shopcartBO);
         // TODO 前端用户在登录的情况下添加商品到购物车会同步添加到redis缓存
+
+        return JSONResult.ok();
+    }
+
+
+    @ApiOperation(value = "从购物车中删除商品", notes = "从购物车中删除商品", httpMethod = "POST")
+    @PostMapping("/del")
+    public JSONResult del(
+            @RequestParam String userId,
+            @RequestParam String itemSpecId,
+            HttpServletRequest request,
+            HttpServletResponse response
+    ){
+        if(StringUtils.isBlank(userId) || StringUtils.isBlank(itemSpecId)){
+            return JSONResult.errorMsg("参数不能为空");
+        }
+
+        // TODO 用户在页面删除购物车中的商品数据,如果用户已经登录,则需要同步删除后端购物车中的商品
 
         return JSONResult.ok();
     }
